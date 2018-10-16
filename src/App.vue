22aad9689b52ad0824c7cad27ea9923599cb196b
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2018-10-16 11:05:33 -0300

    Página Inicial
    Criação da Tela

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="login">
+      <div>
+         <app-login v-if="isLogged == false"> </app-login>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Login from './components/Login.vue'
+export default {
+    data () {
+      return {
+        isLogged: false
+      }
+    },
+    methods: {
+    
+    },
+    components: {
+      appLogin: Login
+    }
+}
+</script>
+
+
+<style>
+  body {
+    background-color: #E8E9F3;
+  }
+  .login {
+    background-color: #cecece;
+    position: relative;
+    margin: 15% auto;
+    padding: 2.5%;
+  }
+</style>
+
+
+
---
+++ src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <h1 class="text-center">Sistema de Indicadores</h1>
+    <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Usuário</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Usuário">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Senha</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+  </div>
+  <button type="submit" class="btn btn-primary" style="background-color: #272635; margin:1%">Login</button>
+  <button type="submit" class="btn btn-primary" style="background-color: #272635; margin:1%">Cadastrar</button>
+</form>
+</div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style>
+
+</style>
