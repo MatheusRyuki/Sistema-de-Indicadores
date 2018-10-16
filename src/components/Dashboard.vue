6fc089e700cbab23d42d42b22f12a09bf1095398
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2018-10-16 12:25:52 -0300

    Dashboard
    Já foi iniciado o dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,6 +2,15 @@
     <div>
         <h1 class="text-center">Sistema de Indicadores</h1>
         <h6>Bem vindo {{this.$parent.usuario}}!</h6>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="../assets/bar-chart.svg" alt="Card image cap">
+            <hr>
+            <div class="card-body">
+                <h5 class="card-title">Planilha Geral</h5>
+                <p class="card-text">Verificar a Planilha Geral dos Indicadores</p>
+                <a href="#" class="btn btn-primary">Planilha Geral</a>
+            </div>
+        </div>
     </div>
 </template>
 
